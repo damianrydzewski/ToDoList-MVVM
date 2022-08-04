41773087eea800f8762d2ec0f9f4a245aa1815e2
Damian <damianrydzewski.dev@gmail.com>
2022-08-04 18:04:54 +0200

    Initial Commit

---
+++ ToDoList-MVVM/ToDoList_MVVMApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoList_MVVMApp.swift
+//  ToDoList-MVVM
+//
+//  Created by Damian on 04/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct ToDoList_MVVMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
